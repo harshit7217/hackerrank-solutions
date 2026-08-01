e79e67b6c7cc75249dbdb5afa754d49443feb610
Harshit Chaudhary <146664405+harshit7217@users.noreply.github.com>
2026-08-01 14:27:46 +0530

    ✅ Super Reduced String [java15] — approach1

---
+++ solutions/java15/easy/reduced-string/solution-approach1.java
@@ -0,0 +1,75 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/mountblue-technologies/challenges/reduced-string/problem?isFullScreen=true
+// Problem     Super Reduced String
+// Difficulty  Easy
+// Subdomain   Algorithms
+// Platform    HackerRank
+// Language    java15
+// Status      Accepted
+// Submitted   2026-08-01, 02:27 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superReducedString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String superReducedString(String s) {
+    // Write your code here
+        Stack<Character> stk = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(stk.isEmpty()){
+                stk.push(c);
+            }else if(stk.peek() == c){
+                stk.pop();
+            }else {
+                stk.push(c);
+            }
+        }
+        if(stk.isEmpty()) return "Empty String";
+        Stack<Character> newStk = new Stack<>();
+        while(!stk.isEmpty()){
+            newStk.push(stk.pop());
+        }
+        String res = "";
+        while(!newStk.isEmpty()){
+            res += newStk.pop();
+        }
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.superReducedString(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
